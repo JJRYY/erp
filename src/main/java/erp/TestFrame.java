4667d2268bad62c697918d9dbb20d0075aa7d505
Ryong <kjr2436@gmail.com>
2021-03-16 12:47:58 +0900

    EmpDetail 작업중

--- src/main/java/erp/TestFrame.java
+++ src/main/java/erp/TestFrame.java
@@ -19,17 +19,15 @@ import erp.ui.content.EmployeePanel;
 import erp.ui.exception.InvalidCheckException;
 import erp.ui.exception.SqlConstraintException;
 import erp.ui.list.EmployeeTablePanel;
+import erp.ui.content.EmployeeDetailPanel;
 
 @SuppressWarnings("serial")
-public class TestFrame extends JFrame implements ActionListener {
+public class TestFrame extends JFrame {
 
 	private JPanel contentPane;
-	private JButton btnAdd;
-	private EmployeePanel pEmpItem;
-	private JButton btnSet;
-	private JButton btnCancel;
 	private EmployeeTablePanel pEmpList;
 	private EmployeeService service;
+	private EmployeeDetailPanel panel;
 
 	public static void main(String[] args) {
 		EventQueue.invokeLater(new Runnable() {
@@ -50,78 +48,21 @@ public class TestFrame extends JFrame implements ActionListener {
 	}
 	private void initialize() {
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 450, 607);
+		setBounds(100, 100, 450, 783);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		setContentPane(contentPane);
 		contentPane.setLayout(new BoxLayout(contentPane, BoxLayout.Y_AXIS));
 		
 		EmployeeService service = new EmployeeService();
-		pEmpItem = new EmployeePanel();
-		pEmpItem.setService(service);
-		contentPane.add(pEmpItem);
 		
 		pEmpList = new EmployeeTablePanel();
 		pEmpList.setService(service);
 		pEmpList.loadData();
 		contentPane.add(pEmpList);
 		
-		JPanel pBtn = new JPanel();
-		contentPane.add(pBtn);
-		
-		btnAdd = new JButton("추가");
-		btnAdd.addActionListener(this);
-		pBtn.add(btnAdd);
-		
-		btnSet = new JButton("Set");
-		btnSet.addActionListener(this);
-		pBtn.add(btnSet);
-		
-		btnCancel = new JButton("취소");
-		btnCancel.addActionListener(this);
-		pBtn.add(btnCancel);
+		panel = new EmployeeDetailPanel();
+		contentPane.add(panel);
 	}
 
-	public void actionPerformed(ActionEvent e) {
-		try {
-			if (e.getSource() == btnCancel) {
-				actionPerformedBtnCancel(e);
-			}
-			if (e.getSource() == btnSet) {
-				actionPerformedBtnSet(e);
-			}
-			if (e.getSource() == btnAdd) {
-				actionPerformedBtnAdd(e);
-			}
-			
-		} catch (InvalidCheckException | SqlConstraintException e1) {
-			JOptionPane.showMessageDialog(null, e1.getMessage());
-		} catch (Exception e1) {
-			e1.printStackTrace();
-		}
-		
-	}
-	protected void actionPerformedBtnAdd(ActionEvent e) {
-		Employee emp = pEmpItem.getItem();
-		service.addEmployee(emp);
-		String message = String.format("%s(%d)%n"
-				+ "title(%s)%n"
-				+ "dept(%s)%n"
-				+ "manager(%s)%n"
-				+ "salary(%,d원)", 
-				emp.getEmpName(), emp.getEmpNo(), emp.getTitle().gettName(), emp.getDept().getDeptName(),
-				emp.getManager().getEmpName(), emp.getSalary());
-		JOptionPane.showMessageDialog(null, message + " 추가했습니다.");
-//		JOptionPane.showMessageDialog(null, emp + " 추가했습니다.");
-		pEmpList.loadData();
-		pEmpItem.clearTf();
-	}
-	protected void actionPerformedBtnSet(ActionEvent e) {
-//		Employee newEmp = pEmpList.getItem();
-		Employee newEmp = new Employee(1003, "조민희", new Title(3), new Employee(4377), 3000000, new Department(2));
-		pEmpItem.setItem(newEmp);
-	}
-	protected void actionPerformedBtnCancel(ActionEvent e) {
-		pEmpItem.clearTf();
-	}
 }
---
+++ src/main/java/erp/dao/EmployeeDetailDao.java
@@ -0,0 +1,11 @@
+package erp.dao;
+
+import erp.dto.Employee;
+
+public interface EmployeeDetailDao {
+	Employee selectEmployeeDetailByNo(Employee employee);
+	
+	int insertEmployeeDetail(Employee employee);
+	int updateEmployeeDetail(Employee employee);
+	int deleteEmployeeDetail(Employee employee);
+}
